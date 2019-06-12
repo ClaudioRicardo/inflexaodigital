51fa4aa235c774170451d992a5efc792f621a12b
claudio_ric_uerj <silverbolt3@gmail.com>
2019-06-12 12:52:13 -0300

    docs(add comentários): Adicionando cometários descritivos dos métodos.

--- posts/a-coleta-como-princípio-da-mineração-de-dados-na-web/main.py
+++ posts/a-coleta-como-princípio-da-mineração-de-dados-na-web/main.py
@@ -5,6 +5,11 @@ from bs4 import BeautifulSoup
 
 
 def get_data(url):
+    '''
+        Função receberá uma url e com a ajuda do método get da bateria
+        requests retornará um documento HTML ou uma mensagem de erro e um
+        return False caso o status_code for diferente de 200.
+    '''
     try:
         result = requests.get(url.strip())
         if result.status_code != 200:
@@ -20,16 +25,27 @@ def get_data(url):
 
 
 def get_table(url):
+    '''
+       A função vai trabalhar com a página html coletada pela função
+       get_data. Com a bateria BeautifulSoup busca-se uma table
+       especifica e extrai-se os dados, alimentando uma matriz com eles.
+    '''
     data_html = get_data(url)
     data_three = BeautifulSoup(data_html.text, "html.parser")
     data_three.prettify()
+
+    #Buscando dentro página uma table com a class="table"  
+
     table = data_three.find("table",{"class": "table"})
     data_table_list = []
 
     ind = 0
+    #Percorrendo a linha
     for line in table.find_all("tr"):
         data_table_list.append([])
+        #Percorrendo as células de cada linha
         for cell in line.find_all({"td","th"}):
+            #Armazenando o texto das células na matriz
             data_table_list[ind].append(cell.text)
         ind+=1
 
@@ -37,7 +53,7 @@ def get_table(url):
 
 
 if __name__=="__main__":
-    url = "http://localhost:8000/posts/a-coleta-como-princ%C3%ADpio-da-minera%C3%A7%C3%A3o-de-dados-na-web/"
+    url = "https://gist.githubusercontent.com/ClaudioRicardo/1f04d5c865c719d50ecd5fe3ada58cab/raw/988ad47bba3bf8b54bd69145abdad555cf309863/tabela_alvo.html"
     print(get_table(url))
 
 
